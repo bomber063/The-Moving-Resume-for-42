7e4dfc22e04117beaa39af1531b8eb3c548ab5e6
bomber <tangyihong063@163.com>
2019-07-24 17:22:44 +0800

    用CSS注释增加一些说明,初步完善CSS

--- main.js
+++ main.js
@@ -3,9 +3,23 @@ var precode=document.querySelector('#precode')
 // var newDiv=document.createElement('div')
 // var body=document.getElementsByTagName('body')
 // body[0].append(newDiv)
-var code=`
-body{
-    background:red;
+var code=`/*面试官你好，我是唐艺轰
+我将以动画的形式来介绍我自己
+
+只用文字介绍太单调了
+我就用代码来介绍吧
+首先准备一些样式*/
+*{
+    transition:all 1s;
+}
+html{
+    color:rgb(222,222,222);
+    background:rgb(0,43,54);
+    font-size:16px;
+}
+#precode{
+    border:1px solid red;
+    padding:16px;
 }
 `
 var n=0
@@ -16,7 +30,7 @@ let id=setInterval(() => {
     if(n>=code.length){
         window.clearInterval(id)
     }
-}, 500);
+}, 10);
 
 
 
